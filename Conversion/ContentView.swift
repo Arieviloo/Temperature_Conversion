cb576386fe529f7cd47987f3adf9caa19507304d
Jadiê Oliveira <jadieoliveira00@gmail.com>
2021-09-07 01:39:08 -0300

    Init temperature

--- Conversion/ContentView.swift
+++ Conversion/ContentView.swift
@@ -8,9 +8,62 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var inputNumber = ""
+    @State private var inputUnit = 0
+    @State private var outputUnit = 1
+    
+    let units = ["Celsius", "Fahrenheit", "Kelvin"]
+    
+    var conversion: Double {
+        
+        let unitInput = units[inputUnit]
+        let unitOutput = units[outputUnit]
+        let value = Double(inputNumber) ?? 0
+        
+        var result = Double()
+        
+        if unitInput == "Celsius" && unitOutput ==  "Fahrenheit"{
+            result = (value * 9/5) + 32
+        }
+        if unitInput == "Celsius" && unitOutput ==  "Kelvin"{
+            result = value + 275.15
+        }
+        
+        
+        
+        return result
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Number", text: $inputNumber).keyboardType(.numberPad)
+                }
+                Section(header: Text("Input unit")) {
+                    Picker("Input unit", selection: $inputUnit) {
+                        ForEach(0 ..< units.count) {
+                            Text("\(self.units[$0])")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                Section(header: Text("Output unit")) {
+                    Picker("Output unit", selection: $outputUnit) {
+                        ForEach(0 ..< units.count) {
+                            Text("\(self.units[$0])")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Text("\(conversion)")
+                
+                
+                
+            }
+            .navigationBarTitle(Text("Temperature"))
+        }
     }
 }
 
