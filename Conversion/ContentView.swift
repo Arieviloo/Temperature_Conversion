2b6f629897669a15ef0af2b85a70044979188029
Jadiê Oliveira <jadieoliveira00@gmail.com>
2021-09-07 18:36:11 -0300

    Add new conversion

--- Conversion/ContentView.swift
+++ Conversion/ContentView.swift
@@ -22,11 +22,34 @@ struct ContentView: View {
         
         var result = Double()
         
+        if unitInput == "Celsius" && unitOutput ==  "Celsius"{
+            result = value
+        }
         if unitInput == "Celsius" && unitOutput ==  "Fahrenheit"{
             result = (value * 9/5) + 32
         }
         if unitInput == "Celsius" && unitOutput ==  "Kelvin"{
-            result = value + 275.15
+            result = value + 273.15
+        }
+        
+        if unitInput == "Fahrenheit" && unitOutput ==  "Fahrenheit"{
+            result = value
+        }
+        if unitInput == "Fahrenheit" && unitOutput ==  "Celsius"{
+            result = (value - 32) * 5/9
+        }
+        if unitInput == "Fahrenheit" && unitOutput ==  "Kelvin"{
+            result = ((value - 32) * 5/9) + 273.15
+        }
+        
+        if unitInput == "Kelvin" && unitOutput ==  "Kelvin"{
+            result = value
+        }
+        if unitInput == "Kelvin" && unitOutput ==  "Fahrenheit"{
+            result = ((value - 273.15) * 9/5) + 32
+        }
+        if unitInput == "Kelvin" && unitOutput ==  "Celsius"{
+            result = value - 273.15
         }
         
         
